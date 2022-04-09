a52d432e095d7ac35eaf294b8e2ad249606b30ca
asstaff2020 <asstaff2020@gmail.com>
2022-04-09 14:19:18 +0900

    메뉴바 show hide 추가

--- index/js/index.js
+++ index/js/index.js
@@ -2,6 +2,18 @@ const mo = document.querySelector("#mo");
 const bg = document.querySelector(".black-bg");
 const cl = document.querySelector("#close"); 
 const ani = document.querySelector(".ani");
+const menuBtn = document.querySelector(".navbar-toggler");
+const menuList = document.querySelector(".list-group");
+
+menuList.style.display="none";
+    
+menuBtn.addEventListener("click", function(){
+    if(menuList.style.display != "block"){
+        menuList.style.display="block";
+    }else{
+        menuList.style.display="none";
+    }
+})
 
 mo.addEventListener("click", function(){
     bg.classList.add("ani");
