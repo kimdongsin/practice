fa880fd01d67a03ed2acd0d734ccda1d97ec38fb
asstaff2020 <asstaff2020@gmail.com>
2022-04-12 03:26:21 +0900

    이자율 계산 문제 풀이

--- index/js/index.js
+++ index/js/index.js
@@ -1,6 +1,6 @@
 const mo = document.querySelector("#mo");
 const bg = document.querySelector(".black-bg");
-const cl = document.querySelector("#close"); 
+const cl = document.querySelector("#close");
 const ani = document.querySelector(".ani");
 const menuBtn = document.querySelector(".navbar-toggler");
 const menuList = document.querySelector(".list-group");
@@ -13,48 +13,64 @@ const userPWD = document.querySelector("#userPWD");
 const dark = document.querySelector("#dark");
 const body = document.querySelector("body");
 
-let count = 0;
+menuList.style.display = "none";
 
-menuList.style.display="none";
-    
-menuBtn.addEventListener("click", function(){
-    if(menuList.style.display != "block"){
-        menuList.style.display="block";
-    }else{
-        menuList.style.display="none";
+menuBtn.addEventListener("click", function () {
+    if (menuList.style.display != "block") {
+        menuList.style.display = "block";
+    } else {
+        menuList.style.display = "none";
     }
 })
 
-mo.addEventListener("click", function(){
+mo.addEventListener("click", function () {
     bg.classList.add("ani");
 })
 
-cl.addEventListener("click", function(){
+cl.addEventListener("click", function () {
     bg.classList.remove("ani");
 })
 
-submitBtn.addEventListener("click", function(e){
-    if(userID.value.length == 0){
+submitBtn.addEventListener("click", function (e) {
+    if (userID.value.length == 0) {
         alert("아이디를 입력해주세요.");
         e.preventDefault();
-    }else if(userPWD.value.length == 0){
+    } else if (userPWD.value.length == 0) {
         alert("비밀번호를 입력해주세요.");
         e.preventDefault();
-    }else if(userPWD.value.length < 6){
+    } else if (userPWD.value.length < 6) {
         alert("비밀번호 6자리 이상 입력해주세요.");
         e.preventDefault();
     }
 })
 
 
-dark.addEventListener("click", function(){
-    count += 1;
-    console.log(count);
-    if(count % 2 != 0){
-        body.classList.add("dark");
-        body.classList.remove("light");
-    }else{
-        body.classList.add("light");
-        body.classList.remove("dark");
-    }
-})
+
+var 예금액 = 10000;
+var 미래예금액 = 0;
+
+if ( 예금액 >= 50000 ){
+  미래예금액 = 예금액 * 1.2 * 1.2 ;
+} else {
+  미래예금액 = 예금액 * 1.15 * 1.15 ;
+}
+
+console.log(미래예금액) 
+
+
+/*
+
+첫 예금액이 5만원 미만이면 이율이 연 15퍼센트,
+
+첫 예금액이 5만원 이상이면 이율이 연 20퍼센트라고 합니다.
+
+
+(동작예시)
+
+var 예금액이라는 변수에
+60000을 집어넣으면 86400 이게 콘솔창에 출력되어야 합니다. (이자 20%가 2번 붙음)
+
+var 예금액이라는 변수에
+10000을 집어넣으면 13225 이게 콘솔창에 출력되어야 합니다. (이자 15%가 2번 붙음)
+
+*/
